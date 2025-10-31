18291ef1a6b9bdb1e71d41bc91b6ac2fd5339043
Damon Cost <dcost@live.com>
2025-10-30 18:27:48 -0700

    Adding db scripts for game scores table, added unique indexs to tables

--- database/nfl_data_tables.sql
+++ database/nfl_data_tables.sql
@@ -6,13 +6,15 @@ create table nfl.teams(
     update_date datetime not null default current_timestamp,
     primary key (id)
     )
-comment = ''
+comment = 'Stores Team Names'
 ;
 use nfl;
 create trigger teams_update_date before update on nfl.teams
 	for each row
     set new.update_date = current_timestamp
 ;
+CREATE UNIQUE INDEX unique_teams_index
+ON teams (location, team_name);
 
 create table nfl.games(
 	id varchar(36) not null default (UUID()),
@@ -24,28 +26,50 @@ create table nfl.games(
     over_under float,
     create_date datetime not null default current_timestamp,
     update_date datetime not null default current_timestamp,
-    primary key (id)
+    primary key (id),
+	foreign key (home_team) references nfl.teams(id),
+	foreign key (away_team) references nfl.teams(id)
     )
-comment = ''
+comment = 'Stores the game and its odds'
 ;
 create trigger games_update_date before update on nfl.games
 	for each row
     set new.update_date = current_timestamp
 ;
+CREATE UNIQUE INDEX unique_games_index
+ON games (game_date, home_team, away_team);
+
+create table nfl.game_scores(
+	id varchar(36) not null default (UUID()),
+    game_id varchar(36),
+    period int,
+    home_score int,
+    away_score int,
+	create_date datetime not null default current_timestamp,
+    update_date datetime not null default current_timestamp,
+    primary key (id),
+	foreign key (game_id) references nfl.games(id)
+    )
+comment = 'Stores the result of a game'
+;
+create trigger game_scores_update_date before update on nfl.game_scores
+	for each row
+    set new.update_date = current_timestamp
+;
+CREATE UNIQUE INDEX unique_game_scores_index
+ON game_scores (game_id, period);
 
 -- TEMPLATE --
 -- create table nfl.(
--- 	id varchar(36) not null default (UUID()),
--- 	create_date datetime not null default current_timestamp,
---     create_user varchar(36) not	null,
---     update_date datetime not null default current_timestamp,
---     update_user varchar(36) not	null,
---     primary key (id),
+-- id varchar(36) not null default (UUID()),
+-- create_date datetime not null default current_timestamp,
+-- update_date datetime not null default current_timestamp,
+-- primary key (id),
 -- foreign key (create_user) references user_acl.user(id),
 -- foreign key (update_user) references user_acl.user(id)
 -- comment = ''
 -- ;
--- create trigger _update_date before update on recipe.
+-- create trigger _update_date before update on nfl.
 -- 	for each row
 --     set new.update_date = current_timestamp
 -- ;
